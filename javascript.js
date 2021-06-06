1fdbe27c4e30780c714474cbadc4d6182426e0d5
Tylerwhitehat <70729291+Tylerwhitehat@users.noreply.github.com>
2021-06-05 21:00:43 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var first_name="Tyler";
+var second_name="Knights";
+console.log(first_name)
+console.log(second_name)
+var full_name=first_name.concat(second_name)
+console.log(full_name)
+var anthony=234;
+var Badanswer=Math.pow(234,3)
+console.log(Badanswer)
